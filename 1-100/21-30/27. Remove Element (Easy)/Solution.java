b927381c3c5e03ace6fadbca8ee0b97cecccca01
Ivan <java.enjoyer@gmail.com>
2021-06-11 14:48:06 +0500

    Create Solution.java

---
+++ 1-100/21-30/27. Remove Element (Easy)/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[len] = nums[i];
+                len++;
+            }
+        }
+        return len;
+    }
+}
+//0ms 37.7mb
