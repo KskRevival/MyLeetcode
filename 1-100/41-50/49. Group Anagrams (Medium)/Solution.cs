f4acdf8bd18df57a93654ee44b1545c5175063a1
Ivan <java.enjoyer@gmail.com>
2022-02-26 21:14:51 +0500

    Create Solution.cs

---
+++ 1-100/41-50/49. Group Anagrams (Medium)/Solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var dict = new Dictionary<string, List<string>>();
+        foreach (var str in strs)
+        {
+            var curr = GetHashedString(str);
+            if (!dict.ContainsKey(curr)) dict.Add(curr, new List<string>());
+            dict[curr].Add(str);   
+        }
+
+        IList<IList<string>> res = dict.Values.Select(value => value).Cast<IList<string>>().ToList();
+
+        return res;
+    }
+
+    public string GetHashedString(string str)
+    {
+        var mas = new int[26];
+        foreach (var c in str) mas[c - 'a']++;
+        return string.Join(" ", mas);
+    }
+}
